923d2cdd20c43892af662bc3f9d8fc32f62ece11
Stoned Elipot <stoned.elipot@gmail.com>
2017-01-24 20:55:21 +0100

    Add sipcalc package

---
+++ packages/sipcalc/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.routemeister.net/projects/sipcalc/
+TERMUX_PKG_DESCRIPTION="advanced console based ip subnet calculator"
+TERMUX_PKG_VERSION=1.1.6
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=http://www.routemeister.net/projects/sipcalc/files/sipcalc-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=cfd476c667f7a119e49eb5fe8adcfb9d2339bc2e0d4d01a1d64b7c229be56357
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
